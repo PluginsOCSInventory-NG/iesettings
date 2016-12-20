b156a8a95733f8bc09f031e0598f93a7c6eb69b7
fbomj <fbomj@users.noreply.github.com>
2016-12-20 21:20:31 +0100

    Add files via upload

---
+++ install.php
@@ -0,0 +1,35 @@
+<?php
+function plugin_version_runningProcess()
+{
+return array('name' => 'Running process',
+'version' => '1.0',
+'author'=> 'Community, Frank BOURDEAU',
+'license' => 'GPLv2',
+'verMinOcs' => '2.2');
+}
+
+function plugin_init_iesettings()
+{
+$object = new plugins;
+$object->add_cd_entry("iesettings","other");
+
+$object->sql_query("CREATE TABLE IF NOT EXISTS `iesttings` (
+                      `ID` INT(11) NOT NULL AUTO_INCREMENT,
+                      `HARDWARE_ID` INT(11) NOT NULL,
+                      `LASTSESSION` VARCHAR(255) DEFAULT NULL,
+                      `SID` VARCHAR(255) DEFAULT NULL,
+                      `PROXYENABLE` INTEGER DEFAULT NULL,
+                      `AUTOCONFIGURL` VARCHAR(255) DEFAULT NULL,
+                      `PROXYSERVER` VARCHAR(15) DEFAULT NULL,
+                      PRIMARY KEY  (`ID`,`HARDWARE_ID`)
+                      ) ENGINE=INNODB;");
+
+}
+
+function plugin_delete_iesettings()
+{
+$object = new plugins;
+$object->del_cd_entry("iesettings");
+$object->sql_query("DROP TABLE `iesettings`");
+
+}
