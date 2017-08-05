037a56fdfe74b67220bb02a49dbca72f2025890a
Stéphane <steph78630@gmail.com>
2017-08-05 10:26:33 +0200

    Add files via upload

--- cd_Iesettings/cd_iesettings.php
+++ cd_Iesettings/cd_iesettings.php
@@ -15,40 +15,42 @@ if(AJAX){
     $protectedPost+=$params;
     ob_start();
     $ajax = true;
-} else {
+}
+else{
     $ajax=false;
 }
-print_item_header("IE settings");
+	print_item_header("IE settings");
 
-if (!isset($protectedPost['SHOW'])){
-    $protectedPost['SHOW'] = 'NOSHOW';
-}
-$form_name="iesettings";
-$table_name=$form_name;
-$tab_options=$protectedPost;
-$tab_options['form_name']=$form_name;
-$tab_options['table_name']=$table_name;
-echo open_form($form_name);
-$list_fields = array(
-    'LASTSESSION' => 'LASTSESSION',
-    'SID' => 'SID',
-    'PROXYENABLE' => 'PROXYENABLE',
-    'AUTOCONFIGURL' => 'AUTOCONFIGURL',
-    'PROXYSERVER' => 'PROXYSERVER',
-    'PROXYOVERRIDE' => 'PROXYOVERRIDE'
-);
-$list_col_cant_del=$list_fields;
-$default_fields= $list_fields;
-$sql=prepare_sql_tab($list_fields);
+	if (!isset($protectedPost['SHOW'])){
+		$protectedPost['SHOW'] = 'NOSHOW';
+	}
+	$form_name="iesettings";
+	$table_name=$form_name;
+	$tab_options=$protectedPost;
+	$tab_options['form_name']=$form_name;
+	$tab_options['table_name']=$table_name;
+	echo open_form($form_name);
+	$list_fields = array(
+		'LASTSESSION' => 'LASTSESSION',
+		'SID' => 'SID',
+		'PROXYENABLE' => 'PROXYENABLE',
+		'AUTOCONFIGURL' => 'AUTOCONFIGURL',
+		'PROXYSERVER' => 'PROXYSERVER',
+		'PROXYOVERRIDE' => 'PROXYOVERRIDE'
+	);
+	$list_col_cant_del=$list_fields;
+	$default_fields= $list_fields;
+	$sql=prepare_sql_tab($list_fields);
 
-$sql['SQL']  .= "FROM $table_name WHERE (hardware_id = $systemid)";
-array_push($sql['ARG'],$systemid);
-$tab_options['ARG_SQL']=$sql['ARG'];
-$tab_options['ARG_SQL_COUNT']=$systemid;
-ajaxtab_entete_fixe($list_fields,$default_fields,$tab_options,$list_col_cant_del);
-echo close_form();
-if ($ajax){
-    ob_end_clean();
-    tab_req($list_fields,$default_fields,$list_col_cant_del,$sql['SQL'],$tab_options);
-    ob_start();
-}
+	$sql['SQL']  .= "FROM $table_name WHERE (hardware_id = $systemid)";
+	array_push($sql['ARG'],$systemid);
+	$tab_options['ARG_SQL']=$sql['ARG'];
+	$tab_options['ARG_SQL_COUNT']=$systemid;
+	ajaxtab_entete_fixe($list_fields,$default_fields,$tab_options,$list_col_cant_del);
+	echo close_form();
+	if ($ajax){
+		ob_end_clean();
+		tab_req($list_fields,$default_fields,$list_col_cant_del,$sql['SQL'],$tab_options);
+		ob_start();
+	}
+?>
