df77bac6defe635cbb90ad72ba0eb507fe209fda
Stéphane <steph78630@gmail.com>
2017-08-05 10:25:56 +0200

    Add files via upload

--- APACHE/Map.pm
+++ APACHE/Map.pm
@@ -1,4 +1,10 @@
-
+###############################################################################
+## OCSINVENTORY-NG
+##
+## This code is open source and may be copied and modified as long as the source
+## code is always made freely available.
+## Please refer to the General Public Licence http://www.gnu.org/ or Licence.txt
+################################################################################
  
 package Apache::Ocsinventory::Plugins::Iesettings::Map;
  
@@ -7,21 +13,20 @@ use strict;
 use Apache::Ocsinventory::Map;
 
 $DATA_MAP{iesettings} = {
-		mask => 0,
-		multi => 1,
-		auto => 1,
-		delOnReplace => 1,
-		sortBy => 'PROXYSERVER',
-		writeDiff => 0,
-		cache => 0,
-		fields => {
+	mask => 0,
+	multi => 1,
+	auto => 1,
+	delOnReplace => 1,
+	sortBy => 'PROXYSERVER',
+	writeDiff => 0,
+	cache => 0,
+	fields => {
                 LASTSESSION => {},
                 SID => {},
                 PROXYENABLE => {},
                 AUTOCONFIGURL => {},
                 PROXYSERVER => {},
                 PROXYOVERRIDE => {}
-	}
+}
 };
-
 1;
